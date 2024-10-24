c5ab348bc7f12f558270ed26dccf35c5a04b6194
Chris <nzeanoruec@gmail.com>
2024-10-24 19:15:18 -0400

    changed the logo name

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = () => {
 		<div>
 			<Link to="/">
 				<img src={LogoImg} alt="Logo" style={{ width: "50px" }} />
-				<span style={{ fontSize: "2rem", fontWeight: "600", fontFamily: "monospace", color: "rgb(89,89,89)" }}>HospitalBridge</span>
+				<span style={{ fontSize: "2rem", fontWeight: "600", fontFamily: "monospace", color: "rgb(89,89,89)" }}>MedRe</span>
 			</Link>
 		</div>
 	);
